9f59972584fca1ed23bf56ce9d9e9a50ee7832a6
Lautab <119816103+Lautab@users.noreply.github.com>
2023-07-13 01:16:48 -0300

    Final

--- main.js
+++ main.js
@@ -16,19 +16,19 @@ menuButton.addEventListener("click", () => {
 const preguntas = [
     {
         id: 0,
-        preg: `A que rama filosofica pertenece esta pregunta: "¿Es posible el conocimiento?"`,
+        preg: `A que rama filosófica pertenece esta pregunta: "¿Es posible el conocimiento?"`,
         resp: ["Ética", "Gnoseología", "Agnosticismo", "Ninguna opción es correcta"],
         corr: "Gnoseología"
     },
     {
         id: 1,
-        preg: `A que motivación para filosofar se relaciona esto: "Tomar consciencia de la inseguridad del conocimiento humano".`,
+        preg: `¿A qué motivación para filosofar se relaciona esto: "Tomar consciencia de la inseguridad del conocimiento humano"?`,
         resp: ["Tedio", "Asombro", "Situación limite", "Duda"],
         corr: "Duda"
     },
     {
         id: 2,
-        preg: "¿Cual es el extremo opuesto al Excepticismo(Cuestionar)?,",
+        preg: "¿Cuál es el extremo opuesto al Escepticismo?,",
         resp: ["Dogmatismo", "Hedonismo", "Relativismo", "Solipsismo"],
         corr: "Dogmatismo"
     },
@@ -40,7 +40,7 @@ const preguntas = [
     },
     {
         id: 4,
-        preg: "¿Qué filosofo se relaciona principalmente con el empirismo?",
+        preg: "¿Qué filósofo se relaciona principalmente con el empirismo?",
         resp: ["Descartes", "Hume", "Aristóteles", "Platón",],
         corr: "Hume"
     },
@@ -74,52 +74,90 @@ const preguntas = [
         resp: ["Estética","Filosofía política","Metafísica","Ética",],
         corr: "Metafísica"
     },
-    // {
-    //     id: ,
-    //     preg: ,
-    //     resp: ,
-    //     corr: 
-    // },
-    // {
-    //     preg: ,
-    //     resp: ,
-    //     corr: 
-    // },
-    // {
-    //     preg: ,
-    //     resp: ,
-    //     corr: 
-    // },
-    // {
-    //     preg: ,
-    //     resp: ,
-    //     corr: 
-    // },
-    // {
-    //     preg: ,
-    //     resp: ,
-    //     corr: 
-    // },
-    // {
-    //     preg: ,
-    //     resp: ,
-    //     corr: 
-    // },
-    // {
-    //     preg: ,
-    //     resp: ,
-    //     corr: 
-    // },
-    // {
-    //     preg: ,
-    //     resp: ,
-    //     corr: 
-    // },
-    // {
-    //     preg: ,
-    //     resp: ,
-    //     corr: 
-    // },
+    {
+        id: 10,
+        preg: "La función principal de la filosofía es...",
+        resp: ["La realidad","La razón","El pensamiento critico","El saber",],
+        corr: "El pensamiento critico"
+    },
+    {
+        id: 11,
+        preg: "Dios es la garantía del conocimiento, ¿Quién creó esta teoría?",
+        resp: ["Nietzsche","Foucault","Descartes","Platón",],
+        corr: "Descartes"
+    },
+    {
+        id: 12,
+        preg: "¿Qué corriente filosófica asegura que una persona solo conoce lo que la rodea?",
+        resp: ["El Dogmatismo","El Hedonismo","El Racionalismo","El Agnosticismo",],
+        corr: "El Agnosticismo"
+    },
+    {
+        id: 13,
+        preg: "¿Quién creó la alegoría de la caverna?",
+        resp: ["Pitagoras","Platón","Kant","Descartes"],
+        corr: "Platón"
+    },
+    {
+        id: 14,
+        preg: "Existen 2 tipos de hedonismo, el sensible y el corporal",
+        resp: ["Verdadero","Falso"],
+        corr: "Verdadero"
+    },
+    {
+        id: 15,
+        preg: "Se considera empirismo a toda teoría que considere que la experiencia es el origen del conocimiento",
+        resp: ["Verdadero","Falso"],
+        corr: "Verdadero"
+    },
+    {
+        id: 16,
+        preg: "¿Qué es el hombre? Esta pregunta pertenece a la rama de...",
+        resp: ["Gneoseología","Antropología filosófica","Estética","Metafísica"],
+        corr: "Antropología filosófica"
+    },
+    {
+        id: 17,
+        preg: "¿El ... de la filosofía es atemporal?",
+        resp: ["comienzo","origen"],
+        corr: "origen"
+    },
+    {
+        id: 18,
+        preg: "El método de estudio de la filosofía es...",
+        resp: ["La verdad","El Saber","La razón","La realidad"],
+        corr: "La razón"
+    },
+    {
+        id: 19,
+        preg: "¿Que corriente significa investigar cuidadosamente?",
+        resp: ["Solipsismo","Escepticismo","Existencialismo","Racionalismo",],
+        corr: "Escepticismo"
+    },
+    {
+        id: 20,
+        preg:  "¿Qué características son propias del conocimiento científico?",
+        resp: ["Epistemología","Agnosticismo","Racionalismo","Criticismo"],
+        corr: "Epistemología"
+    },
+    {
+        id: 21,
+        preg: "Reflexionar, cuestionar y analizar algún aspecto de la realidad es",
+        resp: ["Objeto","Metodo","Pensamiento Crítico","Praxis Filosófica",],
+        corr: "Praxis Filosófica"
+    },
+    {
+        id: 22,
+        preg: "... nos muestran los limites q tenemos",
+        resp: ["Duda","Situaciones límites","Asombro", "Tedio"],
+        corr: "Situaciones límites"
+    },
+    {
+        id: 23,
+        preg: "El ... busca establecer la posibilidad y los modos en que acontece del mundo y de los otros",
+        resp: ["Problema del ser","Problema del valor","Problema del conocimiento"],
+        corr: "Problema del conocimiento"
+    },
 
 ]
 
